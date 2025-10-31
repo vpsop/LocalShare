f27d324576db8748f468016c268f870db45834c9
Varun Pratap <vpsop544@gmail.com>
2025-10-31 15:37:38 +0530

    add public folder as express.static

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", VIEWS_DIR);
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
